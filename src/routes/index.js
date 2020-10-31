a1442eddcb9dc6aa4a2bab6ec9544d2bd3f84e7b
MOON <muneon96@naver.com>
2020-10-31 16:05:17 +0900

    reset

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,16 +4,9 @@ import styled from "styled-components";
 import HomePage from "../components/HomePage";
 import ProfilePage from "../components/ProfilePage";
 import Contents from "../components/Contents";
-// import ErrorPage from "../components/ErrorPage";
+import ErrorPage from "../components/ErrorPage";
 import AboutPage from "../components/AboutPage";
 
-// function ErrorPage() {
-//   return (
-//     <Container>
-//       <P>찾을 수 없는 페이지입니다.</P>
-//     </Container>
-//   );
-// }
 
 const Container = styled.div`
   display: flex;
@@ -24,20 +17,15 @@ const Container = styled.div`
   width: 100%;
 `;
 
-const P = styled.p`
-  font-size: 30px;
-  color: #ffffff;
-`;
-
 const RootRouter = () => {
   return (
     <Container>
       <Switch>
-        <Route exact path="/MyPortfolio" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
         <Route path="/profilepage" component={ProfilePage} />
         <Route path="/contents" component={Contents} />
         <Route path="/aboutpage" component={AboutPage} />
-        {/* <Route component={ErrorPage} /> */}
+        <Route component={ErrorPage} />
       </Switch>
     </Container>
   );
